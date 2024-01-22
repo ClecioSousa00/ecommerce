cbf77d27149496ff3c04162beaa95a939b91d7d5
ClecioSousa00 <cleciosouza015@gmail.com>
2024-01-22 20:13:07 -0300

    chore: config checkout sttripe

---
+++ src/app/api/order/payment-success/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from 'next/server'
+import Stripe from 'stripe'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2023-10-16',
+})
+
+export const POST = async (request: Request) => {
+  const signature = request.headers.get('stripe-signature')
+
+  if (!signature) {
+    return NextResponse.error()
+  }
+
+  const text = await request.text()
+
+  const event = stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET_KEY,
+  )
+
+  if (event.type === 'checkout.session.completed') {
+    // const session = event.data.object as Stripe.Checkout.Session
+    // console.log({ metadata: session.metadata })
+
+    const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+      event.data.object.id,
+      {
+        expand: ['line_items'],
+      },
+    )
+    const lineItems = sessionWithLineItems.line_items
+    console.log(lineItems)
+
+    // ATUALIZAR PEDIDO
+    // await prismaClient.order.update({
+    //   where: {
+    //     id: session.metadata.orderId,
+    //   },
+    //   data: {
+    //     status: "PAYMENT_CONFIRMED",
+    //   },
+    // });
+  }
+
+  return NextResponse.json({ received: true })
+}
