57ab74fafd197561bfa63a8330a23bbc4afde5ac
ClecioSousa00 <cleciosouza015@gmail.com>
2024-02-03 14:20:58 -0300

    chore: update env

--- src/utils/loadProductsPrisma.ts
+++ src/utils/loadProductsPrisma.ts
@@ -1,38 +1,3 @@
-// import prisma from '@/lib/prisma'
-
-// export constc = async () => {
-//   const categories = await prisma.category.findMany()
-//   // console.log(categories)
-
-//   const productsWitchDiscount = await prisma.product.findMany({
-//     where: {
-//       discountPercentage: { gt: 0 },
-//     },
-//   })
-
-//   const keyboards = await prisma.product.findMany({
-//     where: {
-//       category: {
-//         slug: 'keyboards',
-//       },
-//     },
-//   })
-//   const mouses = await prisma.product.findMany({
-//     where: {
-//       category: {
-//         slug: 'mouses',
-//       },
-//     },
-//   })
-
-//   return {
-//     categories,
-//     productsWitchDiscount,
-//     keyboards,
-//     mouses,
-//   }
-// }
-
 import prisma from '@/lib/prisma'
 
 export const loadProductsPrisma = async () => {
